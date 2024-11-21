78b615a35ab650a21f5d6cbdc4dff08630ca667b
peronimarcos <138503165+peronimarcos@users.noreply.github.com>
2024-11-21 14:13:10 +0000

    atualizando

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
-class Campo{
-    constructor(valor,quantidade){
-    this.valor = valor;
-    this.quantidade = quantidade;
+class Campo {
+    constructor(valor, quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
     }
+}
+
+let lista = [
+    new Campo(7, 3),
+    new Campo(8, 2),
+    new Campo(9, 4),
+    new Campo(10, 7),
+];
+
+function totalElementos() {
+    let totalElementos = 0.0;
+    lista.forEach(item => totalElementos += item.quantidade);
+    return totalElementos;
+}
+
+function adicionar() {
+    let valor = Number(document.getElementById("valor").value);
+    let quantidade = Number(document.getElementById("quantidade").value);
+
+    // Verifica se já existe um campo com o valor
+    let campoExistente = lista.find(campo => campo.valor === valor);
+
+    if (campoExistente) {
+        // Aumenta a quantidade do campo existente
+        campoExistente.quantidade += quantidade;
+    } else {
+        // Adiciona um novo campo se não existir
+        lista.push(new Campo(valor, quantidade));
     }
-    
-    let lista=[
-    new Campo(7,3),
-    new Campo(8,2),
-    new Campo(9,4),
-    new Campo(10,7),
-    ];
-    
-    function adicionar(){
-    let valor=Number(document.getElementById("valor").value);
-    let quantidade=Number(document.getElementById("quantidade").value);
-    
-    const campo=new Campo(valor,quantidade);
-    lista.push(campo);
-    
-    let tabela=document.getElementById("tabela");
-    tabela.innerHTML='';
-    lista.forEach(x=>{
-    tabela.innerHTML+=`
-    <tr>
-    <th scope="row">${x.valor}</th>
-    <td>${x.quantidade}</td>
-    <td>Otto</td>
-    <td>@mdo</td>
-    </tr>
-    `;
+
+    let tabela = document.getElementById("tabela");
+    tabela.innerHTML = '';
+    const total = totalElementos();
+    lista.forEach(x => {
+        const percentual = 100 * x.quantidade / total;
+        const angulo = 360 * x.quantidade / total;
+        tabela.innerHTML += `
+        <tr>
+        <th scope="row">${x.valor}</th>
+        <td>${x.quantidade}</td>
+        <td>${percentual.toFixed(2)}%</td>
+        <td>${angulo.toFixed(2)}º</td>
+        </tr>
+        `;
     });
-    }
+}
